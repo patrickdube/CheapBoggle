5bbe4235c80c4936b7cad102363309cf09e76a31
Patrick Dube <dubepatrick@outlook.com>
2023-03-14 03:03:37 -0400

    refactoring most functions

--- boggle_20031977.py
+++ boggle_20031977.py
@@ -8,128 +8,129 @@
 ## Email: patrick.dube.3@umontreal.ca
 ###############################################################################
 
-# IDEAS
-# - add player struct to store player related attributes and expand to more players?
-# - make it so the grid can be bigger
-# - to track diags in a 4x4: 
-#        middle diag: grid[0][0], grid[1][1], grid[2][2], grid[3][3]
-#        upper+1 diag: grid[0][1], grid[1][2], grid[2][3]
-#        upper+2 diag: grid[0][2], grid[1][3]
-#        upper+3 diag: grid[0][3]
-# - track points and save them for a game with multiple sets (add points to the player struct?)
-# - more than 2 players (so player struct is a good idea here)
-
 # Déclaration des imports et dépendances
 import random
 
 # Déclaration des variables globales, constantes
-grilleGeneree = []
-# listeDesChoisis = []
-# TODO: rajouter 11 des pour un tableau 6x6
-des = {1: 'ETUKNO', 
-       2: 'EVGTIN', 
-       3: 'DECAMP', 
-       4: 'IELRUW', 
-       5: 'EHIFSE', 
-       6: 'RECALS', 
-       7: 'ENTDOS', 
-       8: 'OFXRIA',
-       9: 'NAVEDZ', 
-       10: 'EIOATA',
-       11: 'GLENYU',
-       12: 'BMAQJO',
-       13: 'TLIBRA',
-       14: 'SPULTE',
-       15: 'AIMSOR',
-       16: 'ENHRIS',
-       17: 'ATSIOU',
-       18: 'WIREBC',
-       19: 'QDAHAU',
-       20: 'ACFLNE',
-       21: 'PRSTUG',
-       22: 'JPRXEZ',
-       23: 'EKVYBE',
-       24: 'ALCHEM',
-       25: 'EDUFHK'}
-listeMotsJoueur1 = ['ALLO', 'TEST', 'yo']
-listeMotsJoueur2 = ['supe', 'TEST']
-listeListeMotsJoueurs = [listeMotsJoueur1, listeMotsJoueur2]
-width = 29
-xInitial = 0
-yInitial = 0
+grille = []
+joueurs = []
+liste_mots_joueurs = []
+mots_rejettes = []
+des_choisis = []
+des = {
+    1: 'ETUKNO', 
+    2: 'EVGTIN', 
+    3: 'DECAMP', 
+    4: 'IELRUW', 
+    5: 'EHIFSE', 
+    6: 'RECALS', 
+    7: 'ENTDOS', 
+    8: 'OFXRIA',
+    9: 'NAVEDZ', 
+    10: 'EIOATA',
+    11: 'GLENYU',
+    12: 'BMAQJO',
+    13: 'TLIBRA',
+    14: 'SPULTE',
+    15: 'AIMSOR',
+    16: 'ENHRIS',
+    17: 'ATSIOU',
+    18: 'WIREBC',
+    19: 'QDAHAU',
+    20: 'ACFLNE',
+    21: 'PRSTUG',
+    22: 'JPRXEZ',
+    23: 'EKVYBE',
+    24: 'ALCHEM',
+    25: 'EDUFHK'
+}
 
 # Déclaration des fonctions internes et calculs 
 # avec commentaires détaillés nécessaires seulement (optionnel)
 
 def generer_grille(taille):
-    for row in range(taille):
-        grilleGeneree.append([])
-        # listeDesChoisis est une liste 2D qui contient tous les des choisis par le random.randint()
-        # listeDesChoisis.append([])
-        for _ in range(taille):
-            # taille**2 evite dutiliser un if, car si taille == 4, alors taille**2 == 16 et donc seulement les des 1 a 16 sont pris
-            de = des[random.randint(1, taille**2)]
+    # empeche de prendre un de plus dune fois
+    while len(des_choisis) < taille:
+        de_random = des[random.randint(1, taille**2)]
+        if de_random not in des_choisis:
+            des_choisis.append(de_random)
+    
+    # construction de la grille a partir dune liste de des choisis contenant des des uniques
+    for ligne in range(taille):
+        grille.append([])
+        for de in des_choisis:
             faceDe = de[random.randint(0, 5)]
-            grilleGeneree[row].append(faceDe)
-            # listeDesChoisis[i].append(de)
-    #grilleGeneree = [['a', 'b', 'c', 'd'],['a', 'e', 'i', 'o'],['p','u','o','b'],['y','r','s','n']]
+            grille[ligne].append(faceDe)
 
-    return grilleGeneree
+    return grille
+
+def generer_joueurs(nb_joueurs):
+    for joueur in range(int(nb_joueurs)):
+        liste_mots_joueurs.append([])
+        joueur_actuel = {
+            "numero" : joueur + 1,
+            "mots" : liste_mots_joueurs[joueur]
+        }
+        joueurs.append(joueur_actuel)
+    return joueurs
 
 def afficher_grille(grille):
-    for row in grille:
-        # top line de ----------- a chaque new line
-        print('-'*(len(grille)*4)+'-')
+    for ligne in grille:
+        # top line de ----------- a chaque new line (pattern = (4 * size) + 1)
+        print('-' * (len(grille) * 4) + '-')
         # le | manquant du debut de chaque ligne de la premiere colonne puisquon ne fait que mettre un ' |' apres chaque element (il manque donc celui du debut avant chaque premier element de chaque ligne)
         print('|', end=' ')
-        for mot in row:
-            print(mot + ' |', end=' ')
+        for lettre in ligne:
+            print(lettre + ' |', end=' ')
         print()
     # bottom line de ------------ (vu quon met la top line dans la loop, il va manquer une derniere ligne de -------------)
-    print('-'*(len(grille)*4)+'-')
+    print('-' * (len(grille) * 4) + '-')
 
-def afficher_pointage(grille, numeroJoueur):   
-    listeMotsJoueurActuel = listeListeMotsJoueurs[numeroJoueur - 1]
-    longueurMotLePlusLong = len(max(listeMotsJoueurActuel, key=len))
-    # pourquoi + 18? Pcq jai compte la longueur du reste des caracteres mis a part le mot, et dans notre cas cest 18 (en incluant la decision la plus longue (' -- ILLEGAL ')
-    # TODO: storer le len du restant des caracteres pour eviter de 'hardcoder' une valeur du genre 18
-    longueurNecessaire = longueurMotLePlusLong + 18
+# IDEA: passer un dict joueur en param pour avoir acces a sa liste de mots et tous ses autres attributs
+def afficher_pointage(grille, joueurs):   
     
-    # AFFICHAGE JOUEUR 1-2-3-4-5-...
-    print(f"JOUEUR {numeroJoueur}")
+    # IDEA: faire un for loop pour passer tous les joueurs 
+    # PAR CONTRE: il faut aussi un affichage pour un seul joueur
     
+    mots_joueurs_actuel = liste_mots_joueurs[numero_joueur - 1]
+    longueur_mot_plus_long = len(max(mots_joueurs_actuel, key=len))
+    longueur_base = 29 # longueur dune ligne dans lexemple
+    longueur_reste = 18 # longueur dune ligne sans le mot
+    longueur_totale = longueur_mot_plus_long + longueur_reste
+    
+    print(f"JOUEUR {numero_joueur}")
     # AFFICHAGE -----------------, width de 29 comme dans lexemple du prof (minimum), plus au besoin
-    if longueurNecessaire > width:
-        print('-'*longueurNecessaire)
+    if longueur_totale > longueur_base:
+        print('-'*longueur_totale)
     else:
-        print('-'*width)
+        print('-'*longueur_base)
     
     # AFFICHAGE - MOT       (POINT) -- DECISION
-    for mot in listeMotsJoueurActuel:
+    for mot in mots_joueurs_actuel:
         print('- ', end='')
         
         # mention ILLEGAL
         if est_valide(grille, mot):
-            legalite = ""
+            mention_illegal = ""
         else:
-            legalite = " -- ILLEGAL "
+            mention_illegal = " -- ILLEGAL "
 
         # TODO: implementer la mention -- REJETER
         
         # ajustement de laffichage, centrer en un point fixe
-        if len(mot) == longueurMotLePlusLong:
-            print(mot + ' ' + f'({calcul_point(grille, mot)})' + f'{legalite}')
+        if len(mot) == longueur_mot_plus_long:
+            print(mot + ' ' + f'({calcul_point(grille, mot)})' + f'{mention_illegal}')
         else:
-            print(mot + ' ' + ' '*(longueurMotLePlusLong - len(mot)) + f'({calcul_point(grille, mot)})' + f'{legalite}')
+            print(mot + ' ' + ' '*(longueur_mot_plus_long - len(mot)) + f'({calcul_point(grille, mot)})' + f'{mention_illegal}')
     
     #AFFICHAGE ========
-    if longueurNecessaire > width:
-        print('='*longueurNecessaire)
+    if longueur_totale > longueur_base:
+        print('='*longueur_totale)
     else:
-        print('='*width)
+        print('='*longueur_base)
     
     #AFFICHAGE TOTAL
-    print(f"TOTAL: {calcul_point(grille, listeMotsJoueurActuel)}")
+    print(f"TOTAL: {calcul_point(grille, mots_joueurs_actuel)}")
 
 # TODO: verifier si le mot nest pas deja trouve par un autre joueur -- en faire une autre fonction et lappeler dans jouer()?   
 def est_valide(grille, mot):
@@ -137,7 +138,7 @@ def est_valide(grille, mot):
     # 1. same column
     # 2. same line
     # 3. adjacent letters, but not necessarily same line or same column (BONUS)
-    # 4. le mot nest pas deja touve
+    # 4. le mot nest pas deja trouve
     
     valide = True
     # on verifie que chaque lettre est presente dans la grille
@@ -206,7 +207,6 @@ def est_valide(grille, mot):
         i += 1
     return valide
 
-# Fonction qui calcule soit les points dun seul mot, soit les points dune liste de mots
 def calcul_point(grille, mots):
     total = 0
     
@@ -238,21 +238,71 @@ def calcul_point(grille, mots):
                 total += 5
     return total
 
+def demander_nb_joueurs():
+    nb_joueurs = input("Entrez le nombre de joueurs: ")
+    while not nb_joueurs.isdecimal():
+        nb_joueurs = input("Veuillez entrer un nombre de joueurs entier et positif: ")
+    return nb_joueurs
+
+def demander_taille():
+    taille = input("Entrez la taille de la grille: ")
+    while taille not in "456":
+        taille = input("Veuillez entrer une taille entiere et positive entre 4 et 6: ")
+    return int(taille)
+
+def demander_mot():
+    return "test"
+
+# QUESTION: dans le cas où on a plus que 2 joueurs, est-ce que le mot est rejetté si tous les autres joueurs décident de le rejetter, ou lorsqu'un seul joueur décide de le rejetter?
+def demander_rejection(mot, index_joueur_actuel):
+    compte_rejets = 0
+    
+    for i, joueur in enumerate(joueurs):
+
+        if i == index_joueur_actuel:
+            continue
+
+        numero = joueur["numero"]
+        rejet = input(f"JOUEUR {numero} - Rejetter le mot {mot}? [O/N]: ")
+        while rejet.upper() not in "ON":
+            rejet = input(f"JOUEUR {numero} - Rejetter le mot {mot}? [O/N]: ")
+
+        # si le rejet est fait lorsqu'un seul joueur rejette
+        # if rejet == "O":
+        #     mot_rejette = True
+        
+        # si le rejet est fait lorsque tous les joueurs rejettent
+        if rejet == "O":
+            compte_rejets += 1
+    
+    # selon la méthode de rejection dans QUESTION 
+    # s'il y a rejection: on ajoute le mot dans une liste
+    # on pourra ensuite utiliser cette liste pour déterminer quels sont les mots rejettés et afficher en conséquence   
+    if compte_rejets >= len(joueurs) - 1:
+        mots_rejettes.append(mot)
+
+def demander_poursuivre():
+    return False
+
 def jouer():
-    tailleGrille = input("Entrez la taille du jeu (par exemple, taille 6 génère un jeu 6x6): ")
-    # nbJoueurs = input("Entrez le nombre de joueurs: ")
-    nbManches = input("Entrez le nombre de manches: ")
-    # 2. generer la grille
-    grilleGeneree = generer_grille(tailleGrille)
-    # 3. afficher la grille
-    afficher_grille(grilleGeneree)
-    # 4. JOUER TOUR
-        # 4.1 joueur entre mot (demander si le joueur veut entrer un mot)
-        # 4.2 autres joueurs rejettent ou non le mot
-        # FIN TOUR quand len(mots) de tous les joueurs == 10 (si len(mots) dun joueur == 10, il ne peut plus entrer de mots jusqua ce que la partie se termine)
-        # FIN TOUR quand tous les joueurs choisissent de ne plus entrer de mot
-    # 5. Calculer les points de chaque joueur
-    # 6. Manche suivante, retour au point 4.
+    taille = demander_taille()
+    grille = generer_grille(taille)
+    nb_joueurs = demander_nb_joueurs()
+    joueurs = generer_joueurs(nb_joueurs)
+    joueurs_inactifs = []
+    partie_en_cours = True
+
+    while partie_en_cours:
+        for i, joueur in enumerate(joueurs):
+            if joueur not in joueurs_inactifs:  
+                mot_actuel = demander_mot()
+            demander_rejection(mot_actuel, i)
+            demander_poursuivre()
+            if not demander_poursuivre() or len(joueur["mots"]) >= 10:
+                joueurs_inactifs.append(joueur)
+            if len(joueurs_inactifs) >= len(joueurs):
+                partie_en_cours = False
+                break
     return
 
 def test():
@@ -270,9 +320,14 @@ def test_generer_grille():
     #         assert grille[i][j] in listeDesChoisis[i][j], f"la lettre a la pos ({i}, {j}) nest pas contenue dans le de {i+1}"
 
 # Déclaration du code principal et Affichage
-grille = generer_grille(4)
-afficher_grille(grille)
-afficher_pointage(grille, 2)
+
+#grille = generer_grille(4)
+#joueurs = generer_joueurs(2)
+#afficher_grille(grille)
+#afficher_pointage(grille, joueurs)
+jouer()
+#print(joueurs)
 #################################################################################
 # Tests (optionnel)
+
 test()
